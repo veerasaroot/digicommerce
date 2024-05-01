8152ead0a09fe0dcba5839650708c482fe134f29
Veerasaroot Bunkangwan <122154002+veerasaroot@users.noreply.github.com>
2024-05-01 16:35:26 +0700

    Finishing the database migrations.

---
+++ app/Models/Order.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function user(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
